72eda2d369796e1269f99c134497dde3ca3a7e05
parthraval22 <113423716+parthraval22@users.noreply.github.com>
2022-09-13 13:46:37 +0530

    Add files via upload

---
+++ storage/framework/views/0c1858122f64e8ed492770a747b65557d1e91fa4.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="<?php echo e(str_replace('_', '-', app()->getLocale())); ?>">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Unsplash</title>
+        <link href="https://cdn.materialdesignicons.com/5.4.55/css/materialdesignicons.min.css" rel="stylesheet">
+
+    </head>
+    <link href="<?php echo e(url('css/app.css')); ?>" rel="stylesheet" />
+    <body class="antialiased">
+        <div id="app">
+            
+        </div>
+        <script src="<?php echo e(url('js/app.js')); ?>" defer></script>
+    </body>
+</html>
+<?php /**PATH /home/vagrant/test/unsplash/resources/views/index.blade.php ENDPATH**/ ?>
